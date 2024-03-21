92652aa954fa0fcfd9c322d9894145a236b17696
shiv.shukla <shiv.shukla@netprophetsglobal.com>
2024-03-21 14:40:33 +0530

    routing changes

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import AuthLayout from './_auth/AuthLayout';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import RootLayout from './_root/RootLayout';
+import Home from './_root/pages/Home';
+import './globals.css';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <main className='flex h-screen'>
+      <Routes>
+        {/* Public Routes - Before Login */}
+        <Route element={<AuthLayout />}>
+
+        </Route>
+        <Route path='/sign-in' element={<SignInForm />} />
+        <Route path='/sign-up' element={<SignUpForm />} />
+
+
+        {/* Private routes - After Login */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
