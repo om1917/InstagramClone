b231962822a034fe8c7359f4df0e8acbb415c235
shiv.shukla <shiv.shukla@netprophetsglobal.com>
2024-03-21 17:58:17 +0530

    changes 2102024

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom"
 
 function AuthLayout() {
+  const isAutheticated = false;
+
   return (
-    <div>AuthLayout</div>
+    <>
+      {
+        isAutheticated ? (
+          <Navigate to={"/"} />
+        ) : (
+          <>
+            <section className="flex flex-1 justify-center items-center flex-col py-10">
+                <Outlet/>
+            </section>
+            <img />
+          </>
+        )
+      }
+    </>
   )
 }
 
